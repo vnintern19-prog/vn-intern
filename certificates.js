232110fad63260aeec2319b563d146c2440d0db7
vnintern19-prog <vnintern19@gmail.com>
2026-08-25 19:15:06 +0530

    Create certificates.js

---
+++ certificates.js
@@ -0,0 +1,15 @@
+const certificates = {
+  "VNINT-2026-001": {
+    name: "DEMO STUDENT",
+    course: "B.Tech",
+    branch: "Computer Science Engineering",
+    college: "Demo College",
+    program: "Internship Program",
+    duration: "1 Month",
+    issueDate: "25 August 2026",
+    certificateNo: "VNINT-2026-001",
+    status: "Valid"
+  }
+
+  // Naya certificate yahan add hoga
+};
